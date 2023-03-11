e6008801b1c0955d8d3007663c4cf012bb9cfe05
RahulSinghParmar <rahulsinghparmar4@gmail.com>
2023-03-11 22:15:38 +0530

    build andriod application support

---
+++ app/src/main/java/the/cube/DelegationService.java
@@ -0,0 +1,14 @@
+package the.cube;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
